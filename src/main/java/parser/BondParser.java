f85b2f8c1e265a39646b5c85a52916673a051a63
Avgur123 <scienide@mail.ru>
2018-03-28 17:04:03 +0300

    New

--- src/main/java/parser/BondParser.java
+++ src/main/java/parser/BondParser.java
@@ -3,8 +3,18 @@ package parser;
 import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -25,6 +35,61 @@ public class BondParser {
     }
 
     public static void main(String[] args) {
+
+        public DataSource dataSource;
+
+        Map<String, List<Map<String, Object>>> excelData = new HashMap<>();
+
+        XSSFWorkbook excelBook = new XSSFWorkbook(new FileInputStream("\\\\VMCOLLECTOR2\\DataIn\\тикеры -  2000-3415.xlsm"));
+
+
+
+        for (int i = 0; i < excelBook.getNumberOfSheets(); i++) {
+            List<Map<String, Object>> currentSheetData = new ArrayList<>();
+
+            XSSFSheet sheet = excelBook.getSheetAt(i);
+            XSSFRow firstRow = sheet.getRow(1);
+
+            if (firstRow.getCell(1).getStringCellValue().contains("RU Equity")) {
+
+                for (int row = 4; row <= sheet.getLastRowNum(); row++) {
+
+                    Map<String, Object> currentRowData = new HashMap<>();
+                    currentRowData.put("ticker", firstRow.getCell(1).toString().substring(0, firstRow.getCell(1).toString().indexOf("RU") - 1));
+
+
+                    if (sheet.getRow(row).getCell(2).toString().isEmpty()) {
+                        break;
+                    }
+                    for (int cell = 2; cell <= 9; cell++) {
+                        try {
+                            if (cell == 2) {
+                                currentRowData.put("source", sheet.getRow(row).getCell(cell));
+                            }
+                            if (cell == 3) {
+                                currentRowData.put("author", sheet.getRow(row).getCell(cell));
+                            }
+                            if (cell == 4) {
+                                currentRowData.put("recommend", sheet.getRow(row).getCell(cell));
+                            }
+                            if (cell == 7) {
+                                if (sheet.getRow(row).getCell(cell).toString().equals("#N/A N/A")) {
+                                    currentRowData.put("targetprice", 0);
+                                } else {
+                                    currentRowData.put("targetprice", sheet.getRow(row).getCell(cell));
+                                }
+                            }
+                            if (cell == 9) {
+                                currentRowData.put("date", sheet.getRow(row).getCell(cell).getDateCellValue());
+                            }
+                        } catch (NullPointerException ignored) {}
+
+                    }
+                    currentSheetData.add(currentRowData);
+                }
+            }
+            excelData.put(firstRow.getCell(1).toString(), currentSheetData);
+        }
 
 
 
